523c1dbdd771011a4e2e78bda28b8c3950cde047
Navarro Munoz, Jorge <jorge.navarromunoz@wur.nl>
2016-08-04 10:34:50 +0200

    Forgot to add functions.py to the last commit
    Former-commit-id: 0327fcfdd3f852c7c6a65dfd100b02aa18c23e1d

--- functions.py
+++ functions.py
@@ -100,15 +100,15 @@ def get_hmm_output_files(output_folder):
 def check_overlap(pfd_matrix, overlap_cutoff):
     """Check if domains overlap for a certain overlap_cutoff.
      If so, remove the domain(s) with the lower score."""
-     
-    row1_count = 0
+    
     delete_list = []
-    for row1 in pfd_matrix:
-        row1_count += 1
-        row2_count = 0
-        for row2 in pfd_matrix:
-            row2_count += 1
-            if row1_count != row2_count and row1[-1] == row2[-1]: #check if we are not comparing the same rows, but has the same CDS
+    for i in range(len(pfd_matrix)-1):
+        for j in range(i+1, len(pfd_matrix)):
+            row1 = pfd_matrix[i]
+            row2 = pfd_matrix[j]
+            
+            #check if we are the same CDS
+            if row1[-1] == row2[-1]:
                 #check if there is overlap between the domains
                 if no_overlap(int(row1[3]), int(row1[4]), int(row2[3]), int(row2[4])) == False:
                     overlapping_nucleotides = overlap(int(row1[3]), int(row1[4]), int(row2[3]), int(row2[4]))
@@ -119,7 +119,7 @@ def check_overlap(pfd_matrix, overlap_cutoff):
                         if float(row1[1]) > float(row2[1]): #see which has a better score
                             delete_list.append(row2)
                         elif float(row1[1]) < float(row2[1]):
-                             delete_list.append(row1)
+                            delete_list.append(row1)
 
     for lst in delete_list:
         try:
@@ -131,15 +131,31 @@ def check_overlap(pfd_matrix, overlap_cutoff):
     # starting/ending positions for the CDS. In this case we need the 
     # absolute start position of each domain so the loci start coordinate
     # needs to be checked
+    absolute_start_positions = [] # we have to take care of strandedness of the origin gene
     for row in pfd_matrix:
         if row[7][0] == "<" or row[7][0] == ">":
             row[7] = row[7][1:]
+        if row[8][0] == "<" or row[8][0] == ">":
+            row[8] = row[8][1:]
+        
+        loci_start = int(row[7])
+        loci_end = int(row[8])
+        domain_start = int(row[3])
+        domain_end = int(row[4])
+        width = 3*(domain_end - domain_start)
+        strand = row[9].split(":")[-1]
+        if strand == "+":
+            absolute_start_positions.append(3*domain_start + loci_start)
+        else:
+            absolute_start_positions.append(loci_end - 3*domain_start - width)
+        
     try:
         # sorted by absolute position: env coordinate start + gene locus start
-        pfd_matrix = sorted(pfd_matrix, key=lambda pfd_matrix_entry: (int(pfd_matrix_entry[3]) + int(pfd_matrix_entry[7])))
+        pfd_matrix = [x for (y,x) in sorted(zip(absolute_start_positions,pfd_matrix), key=lambda pair:pair[0])]
     except ValueError as e:
         print("Something went wrong during the sorting of the fourth column (ValueError): " + str(e))
         print(pfd_matrix)
+        sys.exit()
         
     domains = []
     for row in pfd_matrix:
