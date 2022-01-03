ba3f95bbbca124726aa3ae486406a8636a0679f6
Arjan Draisma <neolook@protonmail.com>
2022-01-03 12:33:59 +0100

    fix pfam exclusion

---
+++ src/big_scape/run/pfam_param.py
@@ -0,0 +1,10 @@
+"""Module containing pfam parameter helper functions and classes
+
+Author: Arjan Draisma
+"""
+
+class PfamParam():
+    """Class which keeps track of run options relating to pfam usage"""
+
+    def __init__(self, options):
+        return
