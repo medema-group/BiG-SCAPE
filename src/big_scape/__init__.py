b29908fc8e63d27d410e884c49bd6b14ea27a092
Arjan Draisma <neolook@protonmail.com>
2022-01-03 15:02:32 +0100

    add more exports

--- src/big_scape/__init__.py
+++ src/big_scape/__init__.py
@@ -1,3 +1,3 @@
 from .network import generate_network, write_network_matrix
 from .clustering import clusterJsonBatch
-from .run.base import Run
+from .run.base import Run, ClusterParam, DirParam, DistParam, GbkParam, MibigParam, NetworkParam, PfamParam
