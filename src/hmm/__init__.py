c8f69f4d1741c859faa02be93d4b7f1b81b95062
Arjan Draisma <neolook@protonmail.com>
2022-01-05 15:48:11 +0100

    move more file processing logic

--- src/hmm/__init__.py
+++ src/hmm/__init__.py
@@ -1,3 +1,4 @@
-from .fileprocessing import find_unprocessed_files, verify_hmm_fasta
+from .fileprocessing import get_cached_fasta_files, get_fasta_files_to_process, check_fasta_files
+from .fileprocessing import get_cached_domtable_files, check_domtable_files, get_processed_domtable_files, get_domtable_files_to_process
 from .hmmalign import launch_hmmalign, run_hmmalign, stockholm_parser
 from .hmmscan import run_hmmscan_multi_threaded, parseHmmScan, check_overlap, runHmmScan, domtable_parser, write_pfd
