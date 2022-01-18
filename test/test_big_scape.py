2874a4e7d0bcbd04d9474946c613f9d3f80b1bfd
Arjan Draisma <arjan.draisma@wur.nl>
2022-01-18 14:56:04 +0100

    add all options

--- test/test_big_scape.py
+++ test/test_big_scape.py
@@ -2,10 +2,38 @@ from unittest import TestCase
 
 from src import big_scape
 
-class MockOptions:
+class test_options:
     """Test options class to replace the generated options class from argument parsing
     """
     label: str
+    inputdir: str
+    outputdir: str
+    pfam_dir: str
+    cores: int
+    include_gbk_str: str
+    exclude_gbk_str: str
+    verbose: bool
+    include_singletons: bool
+    domain_overlap_cutoff: float
+    min_bgc_size: int
+    mix: bool
+    no_classify: bool
+    banned_classes: str
+    cutoffs: float
+    clans: bool
+    clan_cutoff: float
+    hybrids: bool
+    mode: str
+    anchorfile: str
+    force_hmmscan: bool
+    skip_ma: bool
+    mibig21: bool
+    mibig14: bool
+    mibig13: bool
+    mibig_path: str
+    query_bgc: str
+    domain_includelist: bool
+
 
 class TestRunBase(TestCase):
     """Class containing tests for the run object, which tracks run details and keeps timing
@@ -17,7 +45,7 @@ class TestRunBase(TestCase):
         self.test_run.run_mode = "Test"
 
         # test options object
-        self.test_run.options = MockOptions()
+        self.test_run.options = test_options()
         self.test_run.options.label = "Test"
 
     def test_start(self):
