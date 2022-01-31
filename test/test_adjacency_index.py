3ccc1d6c59da3d98ba2c30661453c81d328a5ed3
Arjan Draisma <arjan.draisma@wur.nl>
2022-01-31 18:33:54 +0100

    add adjacency index test

--- test/test_adjacency_index.py
+++ test/test_adjacency_index.py
@@ -1,10 +1,14 @@
 from unittest import TestCase
+from src.big_scape.scores import calc_adj_idx
+from src.big_scape.bgc_dom_info import BgcDomainInfo
+from src.big_scape.distance import calc_ai_pair
 from src.big_scape.bgc_info import BgcInfo
 
 from src import big_scape
+from test.data.generators import create_cluster_couple
 
 class TestAdjacencyIndex(TestCase):
-    """Class containing tests for the run object, which tracks run details and keeps timing
+    """Class containing tests for adjacency index
     """
     test_run: big_scape.Run
 
@@ -15,35 +19,78 @@ class TestAdjacencyIndex(TestCase):
     distinct_bgc_b: BgcInfo
 
     def setUp(self):
-        self.similar_bgc_a = BgcInfo("test_similar_bgc_a")
-        self.similar_bgc_b = BgcInfo("test_similar_bgc_b")
-
-        self.similar_bgc_a.ordered_domain_list = []
-        self.similar_bgc_b.ordered_domain_list = []
-
-        # we'll add 10 domains to these genes
-        for i in range(10):
-            dom_name = "DOM_" + str(i)
-            # bottom 8 are in a
-            if i < 8:
-                self.similar_bgc_a.ordered_domain_list.append(dom_name)
-            # top 8 are in b
-            if i > 1:
-                self.similar_bgc_b.ordered_domain_list.append(dom_name)
+        # similar clusters will have ~80% overlap in domains
+        self.similar_bgc_a, self.similar_bgc_b = create_cluster_couple(True, 15)
         
         # for dissimilar we'll do the same but with less overlap
-        self.distinct_bgc_a = BgcInfo("test_distinct_bgc_a")
-        self.distinct_bgc_b = BgcInfo("test_distinct_bgc_b")
+        self.distinct_bgc_a, self.distinct_bgc_b = create_cluster_couple(False, 15)
 
-        self.distinct_bgc_a.ordered_domain_list = []
-        self.distinct_bgc_b.ordered_domain_list = []
+    def test_adj_index_similar(self):
 
-        # we'll add 10 domains to these genes
-        for i in range(15):
-            dom_name = "DOM_" + str(i)
-            # even are in a
-            if i % 2 == 0:
-                self.distinct_bgc_a.ordered_domain_list.append(dom_name)
-            # odd are in b
-            else:
-                self.distinct_bgc_b.ordered_domain_list.append(dom_name)
+        cluster_a = self.similar_bgc_a
+        cluster_b = self.similar_bgc_b
+
+        a_list = cluster_a.ordered_domain_list
+        b_list = cluster_b.ordered_domain_list
+
+        # normally these values are calculated by the BgcDomainInfo class
+        # but here we just need to know whether the adjacency index works
+        # this is functionally the same as computing the adjacency index for two clusters
+        # where there has been no score expansion
+        a_start = 0
+        b_start = 0
+        a_end = len(cluster_a.ordered_domain_list)
+        b_end = len(cluster_b.ordered_domain_list)
+
+        ai = calc_adj_idx(a_list, b_list, a_start, a_end, b_start, b_end)
+
+        self.assertNotAlmostEqual(ai, 0.0)
+
+    def test_adj_index_distinct(self):
+        cluster_a = self.distinct_bgc_a
+        cluster_b = self.distinct_bgc_b
+
+        a_list = cluster_a.ordered_domain_list
+        b_list = cluster_b.ordered_domain_list
+
+        # normally these values are calculated by the BgcDomainInfo class
+        # but here we just need to know whether the adjacency index works
+        # this is functionally the same as computing the adjacency index for two clusters
+        # where there has been no score expansion
+        a_start = 0
+        b_start = 0
+        a_end = len(cluster_a.ordered_domain_list)
+        b_end = len(cluster_b.ordered_domain_list)
+
+        ai = calc_adj_idx(a_list, b_list, a_start, a_end, b_start, b_end)
+
+        self.assertAlmostEqual(ai, 0.0)
+
+    def test_adj_index_compare(self):
+        cluster_a = self.similar_bgc_a
+        cluster_b = self.similar_bgc_b
+
+        a_list = cluster_a.ordered_domain_list
+        b_list = cluster_b.ordered_domain_list
+
+        a_start = 0
+        b_start = 0
+        a_end = len(cluster_a.ordered_domain_list)
+        b_end = len(cluster_b.ordered_domain_list)
+
+        ai_similar = calc_adj_idx(a_list, b_list, a_start, a_end, b_start, b_end)
+
+        cluster_a = self.distinct_bgc_a
+        cluster_b = self.distinct_bgc_b
+
+        a_list = cluster_a.ordered_domain_list
+        b_list = cluster_b.ordered_domain_list
+
+        a_start = 0
+        b_start = 0
+        a_end = len(cluster_a.ordered_domain_list)
+        b_end = len(cluster_b.ordered_domain_list)
+        
+        ai_distinct = calc_adj_idx(a_list, b_list, a_start, a_end, b_start, b_end)
+
+        self.assertNotAlmostEqual(ai_similar, ai_distinct)
