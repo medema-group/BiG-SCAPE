1573edcc6517cd3b35b3699c52d8c8f51a416302
Arjan Draisma <arjan.draisma@wur.nl>
2022-02-15 15:49:01 +0100

    fix interrupted run error on reading domain fasta

--- src/hmm/__init__.py
+++ src/hmm/__init__.py
@@ -1,5 +1,5 @@
 # fasta
-from .fileprocessing import get_cached_fasta_files, get_fasta_files_to_process, check_fasta_files
+from .fileprocessing import get_cached_fasta_files, get_cached_domain_fasta_files, get_fasta_files_to_process, check_fasta_files
 # domtable
 from .fileprocessing import get_cached_domtable_files, check_domtable_files, get_processed_domtable_files, get_domtable_files_to_process
 # pfd and pfs
