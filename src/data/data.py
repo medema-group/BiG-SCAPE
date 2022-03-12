92df50e6fd18ac669e5d00439d65921ff9c84623
Arjan Draisma <arjan.draisma@wur.nl>
2022-03-12 17:25:58 +0100

    re add load hmms

--- src/data/data.py
+++ src/data/data.py
@@ -178,7 +178,7 @@ def initialize_db(run: Run, database: Database):
         dataset_id, bgc_ids = insert_dataset(database, dataset_name, dataset_meta)
         bgc_ids = insert_dataset_gbks(run, database, dataset_id, dataset_name, dataset_meta, bgc_ids)
 
-    # load_hmms(run, database)
+    load_hmms(run, database)
 
 
 
