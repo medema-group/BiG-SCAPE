da6b96604b12a6d08dc39098ddd88a2b920dffa6
Jorge <jorge.c.navarro.munoz@gmail.com>
2022-03-14 14:19:51 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+     name='BiG-SCAPE',
+     version='1.1.3',
+     py_modules=['functions','ArrowerSVG'],
+     scripts=['bigscape.py'],
+     description='Biosynthetic Genes Similarity Clustering and Prospecting Engine.',
+     keywords=['secondary metabolites', 'natural products', 'genome mining'],
+     url='https://github.com/medema-group/BiG-SCAPE',
+     author='Jorge Navarro',
+     author_email='jorge.c.navarro.munoz@gmail.com',
+     license='GNU Affero v3'
+     )
+
+
