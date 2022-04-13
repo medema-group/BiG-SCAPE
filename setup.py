ef6320d9438c552084cdd7c7fcf3781c04daf96a
Loraine Guéguen <loraine-gueguen@users.noreply.github.com>
2022-04-13 16:53:26 +0200

    add html_template to bin

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+
+def generate_data_files():
+    data_files = []
+    for path, dirs, files in os.walk('html_template'):
+        install_dir = os.path.join('bin',path)
+        list_entry = (install_dir, [os.path.join(path, f) for f in files ])
+        data_files.append(list_entry)
+    return data_files
 
 setup(
      name='BiG-SCAPE',
      version='1.1.3',
      py_modules=['functions','ArrowerSVG'],
      scripts=['bigscape.py'],
-     package_data={'': ['html_template/*',
-                        'html_template/*/*',
-                        'html_template/*/*/*/*',
-                        '*domain*'
-                        ]},
-     include_package_data=True,
+     data_files=generate_data_files(),
      description='Biosynthetic Genes Similarity Clustering and Prospecting Engine.',
      keywords=['secondary metabolites', 'natural products', 'genome mining'],
      url='https://github.com/medema-group/BiG-SCAPE',
