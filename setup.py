0310d3dea5a345ad2f0a79fa7cc5abbf8b614e74
Loraine Guéguen <loraine-gueguen@users.noreply.github.com>
2022-04-13 15:04:11 +0200

    add other needed files in package_data

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ setup(
      version='1.1.3',
      py_modules=['functions','ArrowerSVG'],
      scripts=['bigscape.py'],
+     package_data=['': ['html_template/*',
+                        'html_template/*/*',
+                        'html_template/*/*/*/*',
+                        '*domain*'
+                        ]],
+     include_package_data=True,
      description='Biosynthetic Genes Similarity Clustering and Prospecting Engine.',
      keywords=['secondary metabolites', 'natural products', 'genome mining'],
      url='https://github.com/medema-group/BiG-SCAPE',
