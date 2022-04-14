8e61a7023ed2e1623a82fda11b3bbc2964512154
Loraine Brillet-Guéguen <loraine-gueguen@users.noreply.github.com>
2022-04-14 21:53:19 +0200

    remove unused code

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@
 import os
 from distutils.core import setup
 
-def generate_data_files():
-    data_files = []
-    for directory in ['html_template','Annotated_MIBiG_reference']:
-        for path, dirs, files in os.walk(directory):
-            install_dir = os.path.join('bin',path)
-            list_entry = (install_dir, [os.path.join(path, f) for f in files ])
-            data_files.append(list_entry)
-    data_files.append(('bin', ['anchor_domains.txt','domain_includelist.txt','domains_color_file.tsv']))
-    return data_files
-
 def generate_package_data():
     data_files = []
     for directory in ['html_template','Annotated_MIBiG_reference']:
@@ -31,9 +21,8 @@ setup(
      py_modules=['functions','ArrowerSVG'],
      packages=['BiG-SCAPE'],
      package_dir={'BiG-SCAPE': '.'},
-     scripts=['bigscape.py'],
      package_data=generate_package_data(),
-     #data_files=generate_data_files(),
+     scripts=['bigscape.py'],
      description='Biosynthetic Genes Similarity Clustering and Prospecting Engine.',
      keywords=['secondary metabolites', 'natural products', 'genome mining'],
      url='https://github.com/medema-group/BiG-SCAPE',
