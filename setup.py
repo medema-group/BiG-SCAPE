fca25bf512144f01b9f434613b80d3924aa61d61
Loraine Guéguen <loraine-gueguen@users.noreply.github.com>
2022-04-14 20:08:43 +0200

    refactor setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,27 @@ def generate_data_files():
     data_files.append(('bin', ['anchor_domains.txt','domain_includelist.txt','domains_color_file.tsv']))
     return data_files
 
+def generate_package_data():
+    data_files = []
+    for directory in ['html_template','Annotated_MIBiG_reference']:
+        for path, dirs, files in os.walk(directory):
+            for f in files:
+                data_files.append(os.path.join(path, f))
+    data_files.append('anchor_domains.txt')
+    data_files.append('domain_includelist.txt')
+    data_files.append('domains_color_file.tsv')
+    package_data={'BiG-SCAPE': data_files}
+    return package_data
+
 setup(
      name='BiG-SCAPE',
      version='1.1.4',
      py_modules=['functions','ArrowerSVG'],
+     packages=['BiG-SCAPE'],
+     package_dir={'BiG-SCAPE': '.'},
      scripts=['bigscape.py'],
-     data_files=generate_data_files(),
+     package_data=generate_package_data(),
+     #data_files=generate_data_files(),
      description='Biosynthetic Genes Similarity Clustering and Prospecting Engine.',
      keywords=['secondary metabolites', 'natural products', 'genome mining'],
      url='https://github.com/medema-group/BiG-SCAPE',
