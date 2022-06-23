4ed37214efa8ca2e25d8a0cdbe3c16499d719ae0
Arjan Draisma <arjan.draisma@wur.nl>
2022-06-23 13:25:59 +0200

    pep-8 pass

--- src/big_scape/bgc_collection.py
+++ src/big_scape/bgc_collection.py
@@ -48,70 +48,94 @@ class BgcCollection:
         Inputs:
             bgc_info_dict: bgc info dictionary from gbk.import_gbks
         """
-        for cluster_name in self.bgc_collection_dict:
+        for cluster_name, bgc_collection_info in self.bgc_collection_dict.items():
             if cluster_name in bgc_info_dict:
                 bgc_info = bgc_info_dict[cluster_name]
-                self.bgc_collection_dict[cluster_name].bgc_info = bgc_info
+                bgc_collection_info.bgc_info = bgc_info
             else:
-                # TODO: replace with logger
-                logging.warning("BGC info for %s was not found", cluster_name)
+                logging.warning(
+                    "BGC info for %s was not found",
+                    cluster_name
+                )
 
     def add_source_gbk_files(self, source_gbk_file_dict):
         """Add source GBK files to the collection
 
         Inputs:
             source_gbk_file_dict: source file dict from gbk.import_gbks"""
-        for cluster_name in self.bgc_collection_dict:
+        for cluster_name, bgc_info in self.bgc_collection_dict.items():
             if cluster_name in source_gbk_file_dict:
                 source_gbk_file = source_gbk_file_dict[cluster_name]
-                self.bgc_collection_dict[cluster_name].src_gbk_file = source_gbk_file
+                bgc_info.src_gbk_file = source_gbk_file
             else:
-                # TODO: replace with logger
-                logging.warning("BGC info for %s was not found", cluster_name)
+                logging.warning(
+                    "Source GBK info for %s was not found",
+                    cluster_name
+                )
 
     def add_gene_domain_counts(self, gene_domain_count_dict):
         """Add gene domain counts to bgcs
 
         Inputs:
-            gene_domain_count_dict: {bgc: [domain counts]} dictionary from big_scape.parse_pfd
+            gene_domain_count_dict: {bgc: [domain counts]} dictionary from
+            big_scape.parse_pfd
         """
-        for cluster_name in self.bgc_collection_dict:
+        for cluster_name, bgc_info in self.bgc_collection_dict.items():
             if cluster_name in gene_domain_count_dict:
                 gene_domain_counts = gene_domain_count_dict[cluster_name]
-                self.bgc_collection_dict[cluster_name].num_genes = len(gene_domain_counts)
-                self.bgc_collection_dict[cluster_name].gene_domain_counts = gene_domain_counts
+                bgc_info.num_genes = len(gene_domain_counts)
+                bgc_info.gene_domain_counts = gene_domain_counts
             else:
                 self.bgc_collection_dict[cluster_name].num_genes = 0
-                # TODO: replace with logger
-                logging.warning("Domain count info for %s was not found", cluster_name)
+                logging.warning(
+                    "Domain count info for %s was not found",
+                    cluster_name
+                )
 
     def add_gene_orientations(self, gene_orientation_dict):
         """Add gene orientation information to bgcs
 
         Inputs:
-            gene_orientation_dict: {bgc: [orientations]} dictionary from big_scape.parse_pfd"""
-        for cluster_name in self.bgc_collection_dict:
+            gene_orientation_dict: {bgc: [orientations]} dictionary from
+            big_scape.parse_pfd
+        """
+        for cluster_name, bgc_info in self.bgc_collection_dict.items():
             if cluster_name in gene_orientation_dict:
                 gene_orientations = gene_orientation_dict[cluster_name]
-                self.bgc_collection_dict[cluster_name].gene_orientations = gene_orientations
+                bgc_info.gene_orientations = gene_orientations
             else:
-                # TODO: replace with logger
-                logging.warning("Gene orientation info for %s was not found", cluster_name)
+                logging.warning(
+                    "Gene orientation info for %s was not found",
+                    cluster_name
+                )
 
-    def add_bio_synth_core_pos(self, bio_synth_core_positions):
-        """Add positions of core biosynthetic genes for each bgc to this collection
-        
+    def add_bio_synth_core_pos(self, bio_core_positions):
+        """Add positions of core biosynthetic genes for each bgc to this
+        collection
+
         Inputs:
-            bio_synth_core_positions: {bgc: [positions]} dictionary from big_scape.parse_pfd
+            bio_synth_core_positions: {bgc: [positions]} dictionary from
+            big_scape.parse_pfd
         """
-        for cluster_name in self.bgc_collection_dict:
-            if cluster_name in bio_synth_core_positions:
-                cluster_bio_synth_core_positions = bio_synth_core_positions[cluster_name]
-                self.bgc_collection_dict[cluster_name].bio_synth_core_positions = cluster_bio_synth_core_positions
+        for cluster_name, bgc_info in self.bgc_collection_dict.items():
+            if cluster_name in bio_core_positions:
+                clust_bio_core_positions = bio_core_positions[cluster_name]
+                bgc_info.bio_synth_core_positions = clust_bio_core_positions
             else:
-                # TODO: replace with logger
-                logging.warning("Biosynthetic gene core position info for %s was not found", cluster_name)
+                logging.warning(
+                    "Biosynthetic gene core position info for %s was not found",
+                    cluster_name
+                )
 
     def init_gene_strings(self):
-        for cluster_name in self.bgc_collection_dict:
-            self.bgc_collection_dict[cluster_name].init_gene_string()
+        """Initializes the gene strings for this object"""
+        for cluster_name, bgc_info in self.bgc_collection_dict.items():
+            bgc_info: BgcInfo
+            try:
+                bgc_info.init_gene_string()
+            except (IndexError, TypeError):
+                logging.error(
+                    ("Something went wrong when ititializing gene string for "
+                    "cluster %s"),
+                    cluster_name
+                )
