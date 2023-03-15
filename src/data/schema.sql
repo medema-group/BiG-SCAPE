bc69fa571f6d3d20afb64f9d33d3703871400731
Arjan Draisma <arjan.draisma@wur.nl>
2023-03-15 14:02:10 +0100

    update schema

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -36,13 +36,45 @@ CREATE TABLE IF NOT EXISTS bgc_region_type (
 CREATE TABLE IF NOT EXISTS cds (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     region_id INTEGER NOT NULL,
-    nt_start INTEGER,
-    nt_stop INTEGER,
-    strand INTEGER,
-    locus_tag TEXT,
+    nt_start INTEGER NOT NULL,
+    nt_stop INTEGER NOT NULL,
+    strand INTEGER NOT NULL,
+    locus_tag TEXT NOT NULL,
     protein_id TEXT,
     product TEXT,
-    aa_seq TEXT,
+    aa_seq TEXT NOT NULL,
     UNIQUE(id, region_id),
     FOREIGN KEY(region_id) REFERENCES bgc_region(id)
+)
+
+CREATE TABLE IF NOT EXISTS hsp (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cds_id INTEGER NOT NULL,
+    hmm_id INTEGER NOT NULL,
+    bitscore REAL NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS hsp_alignment (
+    hsp_id INTEGER PRIMARY KEY NOT NULL,
+    model_start INTEGER NOT NULL,
+    model_stop INTEGER NOT NULL,
+    model_gaps TEXT NOT NULL,
+    cds_start INTEGER NOT NULL,
+    cds_stop INTEGER NOT NULL,
+    cds_gaps TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS hmm (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    accession TEXT NOT NULL,
+    name TEXT NOT NULL,
+    db_id INTEGER NOT NULL,
+    model_length INTEGER NOT NULL,
+    FOREIGN KEY(db_id) REFERENCES hmm_db(id)
+)
+
+CREATE TABLE IF NOT EXISTS hmm_db (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    path TEXT NOT NULL,
+    md5 TEXT NOT NULL
 )
