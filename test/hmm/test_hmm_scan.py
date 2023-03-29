8f66ee081e4ef45c93d66639e26c98c27d24dfff
Arjan Draisma <arjan.draisma@wur.nl>
2023-03-29 16:16:29 +0200

    fix strange yield stall

--- test/hmm/test_hmm_scan.py
+++ test/hmm/test_hmm_scan.py
@@ -105,6 +105,6 @@ class TestHMMScan(TestCase):
 
         expected_result = HSP(cds, "PF00457.19", 249.32315063476562)
 
-        actual_result = next(HMMer.hmmsearch_multiprocess([cds]))
+        actual_result = list(HMMer.hmmsearch_multiprocess([cds]))[0]
 
         self.assertEqual(expected_result, actual_result)
