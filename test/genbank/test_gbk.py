f5167427619be93910d4d10916cfde8a5921f809
Arjan Draisma <arjan.draisma@wur.nl>
2023-04-21 13:13:44 +0200

    fix tests

--- test/genbank/test_gbk.py
+++ test/genbank/test_gbk.py
@@ -107,6 +107,79 @@ class TestGBK(TestCase):
 
         self.assertIsInstance(dna_sequence, Seq)
 
+    def test_filter_overlap_over_threshold(self):
+        """Test whether add_cds_filter_overlap correctly throws out a cds"""
+
+        gbk = GBK("", "")
+
+        cds_a = CDS(0, 18)
+        cds_a.aa_seq = "M" * 6
+        cds_a.strand = 1
+
+        cds_b = CDS(0, 9)
+        cds_b.strand = 1
+        cds_b.aa_seq = "M" * 3
+        # nt_overlap_len_a_b = 9
+        # aa_overlap = 9/3 = 3
+        # 10% cds_b aa len = 0.1 * 3 = 0.3
+        # aa_overlap > 10% shortest cds: 3 > 0.3
+
+        gbk.add_cds_overlap_filter(cds_a, 0.1)
+
+        gbk.add_cds_overlap_filter(cds_b, 0.1)
+
+        expected_cds_list = [cds_a]
+
+        self.assertEqual(expected_cds_list, gbk.genes)
+
+    def test_filter_overlap_under_threshold(self):
+        """Test whether filter_overlap correctly includes a new CDS if it overlaps with
+        another CDS but under the cutoff threshold
+        """
+
+        gbk = GBK("", "")
+
+        cds_a = CDS(0, 18)
+        cds_a.aa_seq = "M" * 6
+        cds_b = CDS(18, 36)
+        cds_b.aa_seq = "M" * 4
+        # nt_overlap_len_a_b = 1
+        # aa_overlap = 1/3 = 0.33
+        # 10% cds_b aa len = 0.1*4 = 0.4
+        # aa_overlap < 10% shortest cds: 0.33 < 0.4
+
+        gbk.add_cds_overlap_filter(cds_a, cds_overlap_cutoff=0.1)
+        gbk.add_cds_overlap_filter(cds_b, cds_overlap_cutoff=0.1)
+
+        expected_cds_list = [cds_a, cds_b]
+
+        self.assertEqual(expected_cds_list, gbk.genes)
+
+    def test_filter_overlap_over_threshold_diff_strands(self):
+        """Test whether filter_overlap correclty preserves a CDS that is over the cds
+        cutoff threshold, but is on a different strand
+        """
+
+        gbk = GBK("", "")
+
+        cds_a = CDS(0, 18)
+        cds_a.aa_seq = "M" * 6
+        cds_a.strand = 1
+        cds_b = CDS(0, 9)
+        cds_b.aa_seq = "M" * 3
+        cds_b.strand = -1
+        # nt_overlap_len_a_b = 9
+        # aa_overlap = 9/3 = 3
+        # 10% cds_b aa len = 0.1 * 3 = 0.3
+        # aa_overlap > 10% shortest cds: 3 > 0.3
+
+        gbk.add_cds_overlap_filter(cds_a, cds_overlap_cutoff=0.1)
+        gbk.add_cds_overlap_filter(cds_b, cds_overlap_cutoff=0.1)
+
+        expected_cds_list = [cds_a, cds_b]
+
+        self.assertEqual(expected_cds_list, gbk.genes)
+
     def test_save(self):
         """Tests whether a GBK object is correctly stored in the SQLite database"""
 
