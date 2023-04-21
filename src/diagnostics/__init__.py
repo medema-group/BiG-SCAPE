7097b9da052894705a183182062e7b5bd5adf7fa
Catarina Loureiro <catarinacarolina13@gmail.com>
2023-04-21 15:34:19 +0200

    aded calc_cpu_percent test

--- src/diagnostics/__init__.py
+++ src/diagnostics/__init__.py
@@ -1,6 +1,15 @@
 """Contains modules for diagnostics"""
 
 from src.diagnostics.logger import init_logger, init_logger_file
-from src.diagnostics.profiler import Profiler
+from src.diagnostics.profiler import Profiler, calc_cpu_percent
 
-__all__ = ["init_logger", "init_logger_file", "Profiler"]
+__all__ = [
+    "init_logger",
+    "init_logger_file",
+    "Profiler",
+    "calc_cpu_percent",
+    "get_stats",
+    "child_start_cpu_time",
+    "make_plots",
+    "collect_consumption",
+]
