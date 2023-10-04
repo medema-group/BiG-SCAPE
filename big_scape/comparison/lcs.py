477797d9b521d4c929a4a55c51c0980e1a4e0359
Arjan Draisma <arjan.draisma@wur.nl>
2023-10-04 13:48:10 +0200

    mostly implement lcs for domains

--- big_scape/comparison/lcs.py
+++ big_scape/comparison/lcs.py
@@ -2,13 +2,14 @@
 of domains or CDS in a RecordPair"""
 
 # from python
+from typing import Any
 from difflib import Match, SequenceMatcher
 
 # from other modules
 import big_scape.genbank as bs_genbank
 
 
-def find_lcs(list_a: list, list_b: list) -> tuple[Match, list[Match]]:
+def find_lcs(list_a: list[Any], list_b: list[Any]) -> tuple[Match, list[Match]]:
     """Detect longest common substring using sequencematcher
 
     Args:
@@ -30,7 +31,8 @@ def find_cds_lcs(
     """Find the longest stretch of matching domains between two CDS lists
 
     Args:
-        pair (RecordPair): A RecordPair object
+        a_cds (list[CDS]): List of CDS
+        b_cds (list[CDS]): List of CDS
 
     Returns:
         tuple[int, int, int, int, bool]: a_start, a_stop, b_start, b_stop, reverse
@@ -41,7 +43,7 @@ def find_cds_lcs(
     b_start_fwd = match[1]
     fwd_match_len = match[2]
 
-    # reverse. matching blocks is not used in 1.0 logic
+    # reverse
     match, matching_blocks_rev = find_lcs(a_cds, b_cds[::-1])
     a_start_rev = match[0]
     b_start_rev = match[1]
@@ -90,25 +92,129 @@ def find_cds_lcs(
                 b_start = b_idx
                 b_stop = b_idx + match_len
 
-        for a_idx, b_idx, match_len in matching_blocks_rev:
-            num_domains = len(a_cds[a_idx : a_idx + match_len])  # noqa
-            if num_domains > max:
-                max = num_domains
-
-                reverse = True
-
-                a_start = a_idx
-                a_stop = a_idx + match_len
-
-                b_start = len(b_cds) - b_idx - match_len
-                b_stop = len(b_cds) - b_idx
-
         return a_start, a_stop, b_start, b_stop, reverse
 
     # equal length, but not 1
     # default to forward
     # default to first match
     # TODO: should probably return most central
+    reverse = False
+    a_start = a_start_fwd
+    a_stop = a_start_fwd + fwd_match_len
+
+    b_start = b_start_fwd
+    b_stop = b_start_fwd + fwd_match_len
+
+    return a_start, a_stop, b_start, b_stop, reverse
+
+
+def find_domain_lcs(
+    a_cds: list[bs_genbank.CDS], b_cds: list[bs_genbank.CDS]
+) -> tuple[int, int, int, int, bool]:
+    """Find the longest stretch of matching domains between two lists of domains
+
+    This takes CDS as arguments, but uses the domains within the CDS to find the LCS
+
+    Args:
+        a_cds (list[CDS]): List of CDS
+        b_cds (list[CDS]): List of CDS
+
+    Returns:
+        tuple[int, int, int, int, bool]: a_start, a_stop, b_start, b_stop, reverse
+    """
+    # TODO: use lcs in protocore
+    a_domains = []
+    b_domains = []
+    for cds in a_cds:
+        a_domains.extend(cds.hsps)
+    for cds in b_cds:
+        b_domains.extend(cds.hsps)
+    # forward
+    match, matching_blocks = find_lcs(a_domains, b_domains)
+    a_start_fwd = match[0]
+    b_start_fwd = match[1]
+    fwd_match_len = match[2]
+
+    # reverse
+    match, matching_blocks_rev = find_lcs(a_domains, b_domains[::-1])
+    a_start_rev = match[0]
+    b_start_rev = match[1]
+    rev_match_len = match[2]
+
+    fwd_larger = fwd_match_len > rev_match_len
+    rev_larger = fwd_match_len < rev_match_len
+
+    if fwd_larger:
+        reverse = False
+        a_start = a_start_fwd
+        a_stop = a_start_fwd + fwd_match_len
+
+        b_start = b_start_fwd
+        b_stop = b_start_fwd + fwd_match_len
+
+        return a_start, a_stop, b_start, b_stop, reverse
+
+    if rev_larger:
+        reverse = True
+        a_start = a_start_rev
+        a_stop = a_start_rev + rev_match_len
+
+        b_start = len(b_domains) - b_start_rev - rev_match_len
+        b_stop = len(b_domains) - b_start_rev
+
+        return a_start, a_stop, b_start, b_stop, reverse
+
+    # strategy here is different. if the length is 1, we want to use the most central
+    # match, not the first one
+    if fwd_match_len == 1:
+        # first find which region is shorter in terms of cds
+        a_cds_len = len(a_cds)
+        b_cds_len = len(b_cds)
+
+        # default to A. if B is shorter, use B
+        if a_cds_len <= b_cds_len:
+            use_cds = a_cds
+            use_domains = a_domains
+            matching_block_idx = 0
+        else:
+            use_cds = b_cds
+            use_domains = b_domains
+            matching_block_idx = 1
+
+        # generate a CDS to index dict
+        cds_idx_dict = {cds: i for i, cds in enumerate(use_cds)}
+
+        # go through all LCS matches and find the one with the most central CDS
+        middle = len(use_cds) / 2
+        min = None
+        for matching_block in matching_blocks:
+            # I don't even know why there is a match of len 0 when there are matches
+            # of len 1
+            if matching_block[2] == 0:
+                continue
+
+            idx = matching_block[matching_block_idx]
+
+            domain = use_domains[idx]
+            cds_idx = cds_idx_dict[domain.cds]
+
+            # find the distance to the middle
+            distance = abs(middle - cds_idx)
+
+            if min is None or distance < min:
+                min = distance
+                a_start = matching_block[0]
+                a_stop = matching_block[0] + matching_block[2]
+                b_start = matching_block[1]
+                b_stop = matching_block[1] + matching_block[2]
+
+        return a_start, a_stop, b_start, b_stop, False
+
+    # equal length, but not 1
+    # default to forward
+    # default to first match
+    # TODO: should probably return most central
+
     reverse = False
     a_start = a_start_fwd
     a_stop = a_start_fwd + fwd_match_len
--- test/comparison/test_lcs.py
+++ test/comparison/test_lcs.py
@@ -19,12 +19,16 @@ def generate_random_hsp(cds: bs_genbank.CDS):
     return hsp
 
 
-def generate_common_hsp(cds_a: bs_genbank.CDS, cds_b: bs_genbank.CDS):
+def generate_common_hsp(cds_a: bs_genbank.CDS, cds_b: bs_genbank.CDS, offset=0):
     """Generate a random domain common between a pair of cds"""
     # generate a random string as accession
     accession = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
-    hsp_a = bs_hmmer.HSP(cds_a, accession, 100.0, cds_a.nt_start, cds_a.nt_stop)
-    hsp_b = bs_hmmer.HSP(cds_b, accession, 100.0, cds_b.nt_start, cds_b.nt_stop)
+    hsp_a = bs_hmmer.HSP(
+        cds_a, accession, 100.0, cds_a.nt_start + offset, cds_a.nt_stop + offset
+    )
+    hsp_b = bs_hmmer.HSP(
+        cds_b, accession, 100.0, cds_b.nt_start + offset, cds_b.nt_stop + offset
+    )
 
     return hsp_a, hsp_b
 
@@ -37,8 +41,8 @@ def generate_mock_cds_lists(
     if len(common_a) != len(common_b):
         raise ValueError("common_a and common_b must be of the same length")
 
-    cds_a = []
-    cds_b = []
+    cds_a: list[bs_genbank.CDS] = []
+    cds_b: list[bs_genbank.CDS] = []
     for i in range(max(len_a, len_b)):
         if i < len_a:
             a = bs_genbank.CDS(i * 100, (i + 1) * 100)
@@ -58,13 +62,15 @@ def generate_mock_cds_lists(
         common_b_idx = common_b[common_idx]
 
         common_a_hsp, common_b_hsp = generate_common_hsp(
-            cds_a[common_a_idx], cds_b[common_b_idx]
+            cds_a[common_a_idx], cds_b[common_b_idx], common_idx * 10
         )
-        cds_a[common_a_idx].hsps = [common_a_hsp]
-        cds_b[common_b_idx].hsps = [common_b_hsp]
+        cds_a[common_a_idx].hsps.append(common_a_hsp)
+        cds_b[common_b_idx].hsps.append(common_b_hsp)
 
     if reverse:
         cds_b = cds_b[::-1]
+        for cds in cds_b:
+            cds.hsps = cds.hsps[::-1]
 
     return cds_a, cds_b
 
@@ -135,3 +141,79 @@ class TestCDSLCS(unittest.TestCase):
         lcs = bs_comparison.lcs.find_cds_lcs(cds_a, cds_b)
 
         self.assertEqual(lcs, (1, 4, 6, 9, True))
+
+
+class testDomainLCS(unittest.TestCase):
+    """Tests for the lcs module using domain lists."""
+
+    def test_lcs_domains_forward(self):
+        """Test lcs detection for two domain lists"""
+        cds_a, cds_b = generate_mock_cds_lists(
+            10, 10, [1, 2, 2, 2, 3], [1, 2, 2, 2, 3], False
+        )
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        self.assertEqual(lcs, (1, 6, 1, 6, False))
+
+    def test_lcs_domains_reverse(self):
+        """Test lcs detection for two domain lists, reverse"""
+        cds_a, cds_b = generate_mock_cds_lists(
+            10, 10, [1, 2, 2, 2, 3], [1, 2, 2, 2, 3], True
+        )
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        self.assertEqual(lcs, (1, 6, 6, 11, True))
+
+    def test_lcs_domains_empty(self):
+        """Test lcs detection for two empty domain lists"""
+        cds_a, cds_b = generate_mock_cds_lists(0, 0, [], [], False)
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        self.assertEqual(lcs, (0, 0, 0, 0, False))
+
+    def test_lcs_domains_len_one(self):
+        """Test lcs detection for two domain lists where there are only matches of len=1"""
+        cds_a, cds_b = generate_mock_cds_lists(10, 10, [1, 3, 5], [1, 3, 5], False)
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        # should return most central, not first
+        self.assertEqual(lcs, (5, 6, 5, 6, False))
+
+    def test_lcs_domains_len_one_reverse(self):
+        """Test lcs detection for two domain lists where there are only matches of
+        len=1, reverse
+        """
+        cds_a, cds_b = generate_mock_cds_lists(10, 10, [1, 3, 5], [1, 3, 6], True)
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        # should return most central, not first
+        self.assertEqual(lcs, (1, 2, 8, 9, False))
+
+    def test_lcs_domains_len_multiple(self):
+        """Test lcs detection for two domain lists where there are equal matches of
+        len>1
+        """
+        cds_a, cds_b = generate_mock_cds_lists(
+            10, 10, [1, 2, 2, 3, 5, 6], [1, 2, 2, 3, 5, 6], False
+        )
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        self.assertEqual(lcs, (1, 5, 1, 5, False))
+
+    def test_lcs_domains_len_multiple_reverse(self):
+        """Test lcs detection for two domain lists where there are equal matches of
+        len>1, reverse
+        """
+        cds_a, cds_b = generate_mock_cds_lists(
+            10, 10, [1, 2, 2, 3, 5, 6], [1, 2, 2, 3, 5, 6], True
+        )
+
+        lcs = bs_comparison.lcs.find_domain_lcs(cds_a, cds_b)
+
+        self.assertEqual(lcs, (1, 5, 6, 10, True))
