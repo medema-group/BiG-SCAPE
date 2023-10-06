fe76e7abf4bc2dcf77e2d9447be487e5735a9c48
nlouwen <nicolouwen@hotmail.com>
2023-10-06 09:48:52 +0200

    Initialize main benchmarking workflow

---
+++ benchmark/main.py
@@ -0,0 +1,14 @@
+"""Main benchmarking workflow"""
+
+# from python
+
+
+# from dependencies
+
+# from other modules
+
+
+def run_benchmark() -> None:
+    """Run a benchmarking workflow. This will (if needed) run BiG-SCAPE on provided
+    input data and evaluate computed GCFs based on a provided curated GCFs"""
+    pass
