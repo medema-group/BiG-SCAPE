7d487c91c6cc78c95251df4a2f2e48cc852b5947
nlouwen <nicolouwen@hotmail.com>
2023-10-06 13:12:19 +0200

    load original filename from database

--- benchmark/data/load_data.py
+++ benchmark/data/load_data.py
@@ -25,22 +25,27 @@ class BenchmarkData:
             _ = inf.readline()
             data = {}
             for line in inf:
-                bgc, family = line.strip().split("t")
+                bgc, family = line.strip().split("\t")
                 data[bgc] = family
             self.curated_labels = data
 
+    # TODO: loading runs with multiple GCF cutoffs
     def load_computed_labels(self):
-        """Load copmuted GCF assignments from database"""
+        """Load computed GCF assignments from database"""
         DB.load_from_disk(self.db_path)
         fam_table = DB.metadata.tables["bgc_record_family"]
+        record_table = DB.metadata.tables["bgc_record"]
+        gbk_table = DB.metadata.tables["gbk"]
 
         select_query = (
-            fam_table.select()
-            .add_columns(fam_table.c.record_id, fam_table.c.family)
+            fam_table.join(record_table, fam_table.c.record_id == record_table.c.id)
+            .join(gbk_table, record_table.c.gbk_id == gbk_table.c.id)
+            .select()
+            .add_columns(fam_table.c.record_id, fam_table.c.family, gbk_table.c.path)
             .compile()
         )
 
         cursor_results = DB.execute(select_query)
         self.computed_labels = {
-            result.record_id: result.family for result in cursor_results
+            Path(result.path).stem: result.family for result in cursor_results
         }
