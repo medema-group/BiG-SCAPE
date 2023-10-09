a5b6983d81280589ed011636645dae6f53a175de
nlouwen <nicolouwen@hotmail.com>
2023-10-09 14:42:01 +0200

    Add association fractions

--- benchmark/metrics/gcf_evaluation.py
+++ benchmark/metrics/gcf_evaluation.py
@@ -13,6 +13,21 @@ class BenchmarkMetrics:
     """Class to store evaluation metric calculation functions"""
 
     @staticmethod
+    def create_fam_index(labels: dict[str, str]) -> dict[str, list[str]]:
+        """Create family index linking each family to their BGC members
+
+        Args:
+            labels (dict[str, str]): dict linking each BGC to their assigned family
+
+        Returns:
+        Dictionary linking each family to a list of their BGC members
+        """
+        fam_index: dict[str, list[str]] = {}
+        for bgc, fam in labels.items():
+            fam_index.setdefault(fam, []).append(bgc)
+        return fam_index
+
+    @staticmethod
     def calculate_v_measure(
         curated_labels: dict[str, str], computed_labels: dict[str, str]
     ) -> float:
@@ -24,6 +39,7 @@ class BenchmarkMetrics:
         Args:
             curated_labels (dict[str, str]): curated GCF assignments per BGC
             computed_labels (dict[str, str]): BiG-SCAPE computed GCF assignments per BGC
+
         Returns:
             float: V-measure
         """
@@ -46,12 +62,11 @@ class BenchmarkMetrics:
         Args:
             curated_labels (dict[str, str]): curated GCF assignments per BGC
             computed_labels (dict[str, str]): BiG-SCAPE computed GCF assignments per BGC
+
         Returns:
             dict[str, float]: dictionary containing each family and their purity
         """
-        computed_families: dict[str, list[str]] = {}
-        for bgc, family in computed_labels.items():
-            computed_families.setdefault(family, []).append(bgc)
+        computed_families = BenchmarkMetrics.create_fam_index(computed_labels)
 
         purities: dict[str, float] = {}
         for family, bgcs in computed_families.items():
@@ -69,12 +84,11 @@ class BenchmarkMetrics:
         Args:
             curated_labels (dict[str, str]): curated GCF assignments per BGC
             computed_labels (dict[str, str]): BiG-SCAPE computed GCF assignments per BGC
+
         Returns:
             dict[str, float]: dictionary containing each family and their entropy
         """
-        computed_families: dict[str, list[str]] = {}
-        for bgc, family in computed_labels.items():
-            computed_families.setdefault(family, []).append(bgc)
+        computed_families = BenchmarkMetrics.create_fam_index(computed_labels)
 
         entropies: dict[str, float] = {}
         for family, bgcs in computed_families.items():
@@ -85,6 +99,53 @@ class BenchmarkMetrics:
             ]
             entropies[family] = entropy(pk, base=2)
         return entropies
+
+    @staticmethod
+    def compare_association(
+        curated_labels: dict[str, str], computed_labels: dict[str, str]
+    ) -> tuple[float, float, float, float]:
+        """Collect fractions of correct/wrong and present/missing associations per BGC
+
+        Args:
+            curated_labels (dict[str, str]): curated GCF assignments per BGC
+            computed_labels (dict[str, str]): BiG-SCAPE computed GCF assignments per BGC
+
+        Returns:
+            Fractionw of correct and wrong associations per BGC based on computed
+            families, and fraction of present and missing associations based on curated
+            families.
+        """
+        computed_bgcs = computed_labels.keys()
+        curated_labels_used = {
+            bgc: fam for bgc, fam in curated_labels.items() if bgc in computed_bgcs
+        }
+        computed_fams = BenchmarkMetrics.create_fam_index(computed_labels)
+        curated_fams = BenchmarkMetrics.create_fam_index(curated_labels_used)
+
+        correct = []
+        wrong = []
+        present = []
+        missing = []
+        for bgc in computed_bgcs:
+            cur_assoc = set(curated_fams[curated_labels_used[bgc]])
+            comp_assoc = set(computed_fams[computed_labels[bgc]])
+            # remove self-association
+            cur_assoc.remove(bgc)
+            comp_assoc.remove(bgc)
+
+            correct.append(len(cur_assoc.intersection(comp_assoc)) / len(comp_assoc))
+            wrong.append(len(comp_assoc - cur_assoc) / len(comp_assoc))
+            present.append(len(cur_assoc.intersection(comp_assoc)) / len(cur_assoc))
+            missing.append(
+                len([m for m in cur_assoc if m not in comp_assoc]) / len(cur_assoc)
+            )
+
+        return (
+            float(np.average(correct)),
+            float(np.average(wrong)),
+            float(np.average(present)),
+            float(np.average(missing)),
+        )
 
     @staticmethod
     def calculate_summary(
