72a2b49ced5fe1ac8f0b32b787bd289f7db6f715
Arjan Draisma <arjan.draisma@wur.nl>
2023-10-10 10:22:31 +0200

    removed unused function add pragma for no cover

--- big_scape/comparison/lcs.py
+++ big_scape/comparison/lcs.py
@@ -58,7 +58,7 @@ def find_protocore_distance(region: bs_genbank.ProtoCluster, idx: int) -> int:
             min_dist = dist
 
     if min_dist is None:
-        raise ValueError("No protocore found")
+        raise ValueError("No protocore found")  # pragma: no cover # should never happen
 
     return min_dist
 
@@ -192,93 +192,6 @@ def get_lcs_protocores(
     return a_start, a_stop, b_start, b_stop, False
 
 
-def find_cds_lcs_region(
-    a_cds: list[bs_genbank.CDS], b_cds: list[bs_genbank.CDS]
-) -> tuple[int, int, int, int, bool]:
-    """Find the longest stretch of matching domains between two CDS lists
-
-    If there are LCS of the same length, the LCS closest to the middle of the region
-    is preferred (TODO)
-    TODO: maybe this is not useful at all
-
-    Args:
-        a_cds (list[CDS]): List of CDS
-        b_cds (list[CDS]): List of CDS
-
-    Returns:
-        tuple[int, int, int, int, bool]: a_start, a_stop, b_start, b_stop, reverse
-    """
-    # forward
-    match, matching_blocks = find_lcs(a_cds, b_cds)
-    a_start_fwd = match[0]
-    b_start_fwd = match[1]
-    fwd_match_len = match[2]
-
-    # reverse
-    match, matching_blocks_rev = find_lcs(a_cds, b_cds[::-1])
-    a_start_rev = match[0]
-    b_start_rev = match[1]
-    rev_match_len = match[2]
-
-    fwd_larger = fwd_match_len > rev_match_len
-    rev_larger = fwd_match_len < rev_match_len
-
-    if fwd_larger:
-        reverse = False
-        a_start = a_start_fwd
-        a_stop = a_start_fwd + fwd_match_len
-
-        b_start = b_start_fwd
-        b_stop = b_start_fwd + fwd_match_len
-
-        return a_start, a_stop, b_start, b_stop, reverse
-
-    if rev_larger:
-        reverse = True
-        a_start = a_start_rev
-        a_stop = a_start_rev + rev_match_len
-
-        b_start = len(b_cds) - b_start_rev - rev_match_len
-        b_stop = len(b_cds) - b_start_rev
-
-        return a_start, a_stop, b_start, b_stop, reverse
-
-    # equal lengths
-
-    # length of 1. use the matching block with the most domains
-    # if all equal, this returns the first
-    # TODO: should probably return most central
-    if fwd_match_len == 1:
-        max = 0
-        for a_idx, b_idx, match_len in matching_blocks:
-            num_domains = len(a_cds[a_idx : a_idx + match_len])  # noqa
-            if num_domains > max:
-                max = num_domains
-
-                reverse = False
-
-                a_start = a_idx
-                a_stop = a_idx + match_len
-
-                b_start = b_idx
-                b_stop = b_idx + match_len
-
-        return a_start, a_stop, b_start, b_stop, reverse
-
-    # equal length, but not 1
-    # default to forward
-    # default to first match
-    # TODO: should probably return most central
-    reverse = False
-    a_start = a_start_fwd
-    a_stop = a_start_fwd + fwd_match_len
-
-    b_start = b_start_fwd
-    b_stop = b_start_fwd + fwd_match_len
-
-    return a_start, a_stop, b_start, b_stop, reverse
-
-
 def find_domain_lcs_region(
     a_cds: list[bs_genbank.CDS], b_cds: list[bs_genbank.CDS]
 ) -> tuple[int, int, int, int, bool]:
@@ -399,7 +312,7 @@ def find_domain_lcs_region(
 def find_domain_lcs_protocluster(
     pair: bs_comparison.RecordPair,
 ) -> tuple[int, int, int, int, bool]:
-    """Find the longest stretch of matching domains between two protocluster regions,
+    """Find the longest stretch of matching domains between two protocluster records,
     using domains
 
     Args:
