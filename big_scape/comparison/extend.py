b1b5ddec8181a1acf5f4829804e9e10cf3f438f7
Arjan Draisma <arjan.draisma@wur.nl>
2023-10-17 15:54:41 +0200

    implement check_expand

--- big_scape/comparison/extend.py
+++ big_scape/comparison/extend.py
@@ -20,14 +20,41 @@ def reset_expansion(comparable_region: ComparableRegion) -> None:
     comparable_region.reverse = False
 
 
-def check_expand(comparable_region: ComparableRegion) -> None:
-    """Checks if a comparable region should be reset after expansion, e.g. if lengths
-    are too short
+def check_expand(comparable_region: ComparableRegion, min_len=3) -> bool:
+    """Checks if a comparable region should be reset after expansion
+
+    returns true if either of the following conditions are met:
+    - the comparable region contains a biosynthetic gene
+    - the comparable region is longer than or equal to min_len
 
     Args:
         comparable_region: The comparable region to check
     """
-    pass
+    if ComparableRegion.cds_range_contains_biosynthetic(
+        comparable_region.pair.region_a,
+        comparable_region.a_start,
+        comparable_region.a_stop,
+        False,
+        False,
+    ):
+        return True
+
+    if ComparableRegion.cds_range_contains_biosynthetic(
+        comparable_region.pair.region_b,
+        comparable_region.b_start,
+        comparable_region.b_stop,
+        False,
+        comparable_region.reverse,
+    ):
+        return True
+
+    a_len = comparable_region.a_stop - comparable_region.a_start
+    b_len = comparable_region.b_stop - comparable_region.b_start
+
+    if a_len >= min_len and b_len >= min_len:
+        return True
+
+    return False
 
 
 def check_lcs(comparable_region: ComparableRegion) -> None:
