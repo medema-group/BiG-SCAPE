b726a8de2a321a102cfac6d219ea6e2f60d3c271
nlouwen <nicolouwen@hotmail.com>
2023-10-23 14:15:20 +0200

    Add multiple cutoffs in family generation

--- big_scape/cluster.py
+++ big_scape/cluster.py
@@ -252,15 +252,19 @@ def run_bigscape(run: dict) -> None:
 
     logging.info("Generating families")
 
-    for connected_component in bs_network.get_connected_components(0.3):
-        logging.debug(
-            "Found connected component with %d edges", len(connected_component)
-        )
+    for cutoff in run["gcf_cutoffs"]:
+        logging.info(" -- Cutoff %s", cutoff)
+        for connected_component in bs_network.get_connected_components(cutoff):
+            logging.debug(
+                "Found connected component with %d edges", len(connected_component)
+            )
 
-        regions_families = bs_families.generate_families(connected_component)
+            regions_families = bs_families.generate_families(
+                connected_component, cutoff
+            )
 
-        # save families to database
-        bs_families.save_to_db(regions_families)
+            # save families to database
+            bs_families.save_to_db(regions_families)
 
     DB.commit()
 
