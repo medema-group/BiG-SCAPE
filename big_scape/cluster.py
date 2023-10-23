d8e98534a2973c40d1a5bd71360653150e7c0d3c
nlouwen <nicolouwen@hotmail.com>
2023-10-23 14:40:38 +0200

    Add multiple cutoffs output

--- big_scape/cluster.py
+++ big_scape/cluster.py
@@ -282,7 +282,8 @@ def run_bigscape(run: dict) -> None:
     legacy_prepare_output(run["output_dir"], pfam_info)
 
     # work per cutoff
-    legacy_prepare_cutoff_output(run["output_dir"], run["label"], 0.3, gbks)
+    for cutoff in run["gcf_cutoffs"]:
+        legacy_prepare_cutoff_output(run["output_dir"], run["label"], cutoff, gbks)
 
     # TODO: I don't think the bins make much sense anymore
     # see if we can refactor this
@@ -290,9 +291,9 @@ def run_bigscape(run: dict) -> None:
     mix_bin = bs_comparison.RecordPairGenerator("mix")
     mix_bin.add_records([gbk.region for gbk in gbks if gbk.region is not None])
 
-    legacy_prepare_bin_output(run["output_dir"], run["label"], 0.3, mix_bin)
-
-    legacy_generate_bin_output(run["output_dir"], run["label"], 0.3, mix_bin)
+    for cutoff in run["gcf_cutoffs"]:
+        legacy_prepare_bin_output(run["output_dir"], run["label"], cutoff, mix_bin)
+        legacy_generate_bin_output(run["output_dir"], run["label"], cutoff, mix_bin)
 
     if run["profiling"]:
         profiler.stop()
