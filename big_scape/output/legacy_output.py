b4f54bd8904b1643491ae35f97eb3d25fb25db6c
Arjan Draisma <arjan.draisma@wur.nl>
2023-11-07 15:32:26 +0100

    remove lcs adjust function

--- big_scape/output/legacy_output.py
+++ big_scape/output/legacy_output.py
@@ -875,15 +875,10 @@ def generate_bs_families_alignment(
                 result = fetch_lcs_from_db(
                     family_db_id, bgc_db_id, pair_generator.weights
                 )
-                a_start, b_start, reverse = adjust_lcs_to_all_genes(
-                    result,
-                    family_db_id,
-                    bgc_db_id,
-                    fam_gbk,
-                    bgc_gbk,
-                    domain_genes_to_all_genes,
-                    domain_count_gene,
-                )
+                a_start = result["lcs_a_start"]
+                b_start = result["lcs_b_start"]
+                reverse = result["reverse"]
+
                 ref_genes_.add(a_start)
 
                 bgc_algn = []
