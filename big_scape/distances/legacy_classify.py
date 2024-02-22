5d5e067ff06389b47eb24003b9b8999f94f2056b
Arjan Draisma <arjan.draisma@wur.nl>
2024-02-22 15:34:51 +0100

    lazy fixes to edge generation

--- big_scape/distances/legacy_classify.py
+++ big_scape/distances/legacy_classify.py
@@ -1,6 +1,8 @@
 """contains code for the generation of legacy class bins and calculating
  distances between all records in a given bin"""
 
+# TODO: duplicate code with a lot of other edge generation
+
 # from python
 import logging
 
@@ -34,32 +36,32 @@ def calculate_distances_legacy_classify(
         if num_pairs > 0:
             logging.info("Calculating distances for %d pairs", num_pairs)
 
-            last = 0
-
-            def callback(done_pairs):
-                nonlocal last
-
-                if done_pairs - last > 10000:
-                    last = done_pairs
-
-            legacy_class_edges = bs_comparison.generate_edges(
-                missing_edge_bin, run["alignment_mode"], run["cores"], run["cores"] * 2, callback
-            )
+            save_batch = []
+            num_edges = 0
 
             with tqdm.tqdm(
                 total=num_pairs, unit="edge", desc="Calculating distances"
             ) as t:
-                num_edges = 0
-                save_batch = []
-                batch_size = run["cores"] * 100000
-                for edge in legacy_class_edges:
-                    num_edges += 1
-                    t.update(1)
-                    save_batch.append(edge)
-                    if len(save_batch) > batch_size:
-                        bs_comparison.save_edges_to_db(save_batch)
-                        bs_data.DB.commit()
-                        save_batch = []
+
+                def callback(edges):
+                    nonlocal num_edges
+                    nonlocal save_batch
+                    batch_size = run["cores"] * 100000
+                    for edge in edges:
+                        num_edges += 1
+                        t.update(1)
+                        save_batch.append(edge)
+                        if len(save_batch) > batch_size:
+                            bs_comparison.save_edges_to_db(save_batch, commit=True)
+                            save_batch = []
+
+                bs_comparison.generate_edges(
+                    missing_edge_bin,
+                    run["alignment_mode"],
+                    run["cores"],
+                    run["cores"] * 2,
+                    callback,
+                )
 
             bs_comparison.save_edges_to_db(save_batch)
 
