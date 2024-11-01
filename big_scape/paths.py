9a0275822079f0ee5a3aa67581abd06d803c6d98
Arjan Draisma <arjan.draisma@wur.nl>
2024-11-01 10:23:28 +0100

    re-add underscores. none of this makes sense

--- big_scape/paths.py
+++ big_scape/paths.py
@@ -2,8 +2,8 @@
 
 import pkg_resources
 
-DB_SCHEMA_FILE = pkg_resources.resource_filename("bigscape", "data/schema.sql")
-TEMPLATES_OUTPUT_DIR = pkg_resources.resource_filename("bigscape", "output")
+DB_SCHEMA_FILE = pkg_resources.resource_filename("big_scape", "data/schema.sql")
+TEMPLATES_OUTPUT_DIR = pkg_resources.resource_filename("big_scape", "output")
 # config is in base directory, not in module
 # get path to package directory
-DEFAULT_CONFIG_FILE = pkg_resources.resource_filename("bigscape", "config.yml")
+DEFAULT_CONFIG_FILE = pkg_resources.resource_filename("big_scape", "config.yml")
