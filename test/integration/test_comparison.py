4dda475312dafa86904d2b1d58932e0517f2bef8
nlouwen <nicolouwen@hotmail.com>
2025-01-28 14:31:31 +0100

    simplify setting region and cand_cluster categories

--- test/integration/test_comparison.py
+++ test/integration/test_comparison.py
@@ -114,8 +114,8 @@ def create_mock_complete_single_gbk(
     candidate_cluster.add_proto_cluster(protocluster)
     region.add_cand_cluster(candidate_cluster)
 
-    candidate_cluster.set_record_category(candidate_cluster.get_categories())
-    region.set_record_category(region.get_categories())
+    candidate_cluster.set_record_category()
+    region.set_record_category()
 
     cds = CDS(0, 100)
     cds.parent_gbk = gbk
