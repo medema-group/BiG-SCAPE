cdd616c5a74445a34d19a4eade17a167a0cdfa73
Catarina Loureiro <catarina.salesesantosloureiro@wur.nl>
2025-02-14 16:15:23 +0100

    dereplicate cli

---
+++ big_scape/dereplicating/dereplicate.py
@@ -0,0 +1,10 @@
+"""Main file for BiG-SCAPE dereplicate module"""
+
+# from python
+import logging
+
+# from other modules
+
+
+def run_bigscape_dereplicate(run: dict):
+    logging.info("Will one day run BiG-SCAPE dereplicate")
