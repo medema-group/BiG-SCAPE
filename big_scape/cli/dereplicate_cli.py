eeecf17968a487fea4c9f1f1b856531e72e1aa65
Catarina Loureiro <catarina.salesesantosloureiro@wur.nl>
2025-02-14 16:15:23 +0100

    dereplicate cli

---
+++ big_scape/cli/dereplicate_cli.py
@@ -0,0 +1,50 @@
+""" Click parameters for the BiG-SCAPE Dereplicate CLI command """
+
+# from python
+import click
+
+# from other modules
+from big_scape.dereplicating.dereplicate import run_bigscape_dereplicate
+from big_scape.diagnostics import init_logger, init_logger_file
+
+
+# from this module
+from .cli_common_options import common_all, common_cluster_query_dereplicate
+from .cli_validations import (
+    set_start,
+    validate_output_paths,
+)
+
+
+@click.command()
+@common_all
+@common_cluster_query_dereplicate
+@click.pass_context
+def dereplicate(ctx, *args, **kwargs):
+    """
+    BiG-SCAPE - DEREPLICATE
+
+    Dereplicate mode - BiG-SCAPE performs a pairwise comparison
+    of BGCs based on the protein sequence comparison tool sourmash,
+    clusters them based on a similarity threshold, and selects
+    a representative BGC per cluster.
+    \f
+    :param click.core.Context ctx: Click context.
+    """
+
+    # get context parameters
+    ctx.obj.update(ctx.params)
+    ctx.obj["mode"] = "Dereplicate"
+
+    # set start time and label
+    set_start(ctx.obj)
+
+    # workflow validations
+    validate_output_paths(ctx)
+
+    # initiate logger
+    init_logger(ctx.obj)
+    init_logger_file(ctx.obj)
+
+    # run BiG-SCAPE dereplicate
+    run_bigscape_dereplicate(ctx.obj)
