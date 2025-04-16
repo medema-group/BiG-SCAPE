39256cf6e589952a64f802dafd1af375a0dab085
Catarina Loureiro <catarina.salesesantosloureiro@wur.nl>
2025-04-16 15:35:57 +0200

    add gbk length constraint filtering

--- test/dereplicate/test_gbk.py
+++ test/dereplicate/test_gbk.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from big_scape.dereplicating.gbk_components.gbk import GBK
 import big_scape.enums as bs_enums
 from big_scape.dereplicating.gbk_component_parsing import validate_cds_component
-
+from big_scape.cli.config import BigscapeConfig
 
 class TestGBKComponent(TestCase):
     """Test class for the GBK component"""
@@ -38,3 +38,32 @@ class TestGBKComponent(TestCase):
         gbk = GBK(gbk_file_path, gbk_hash, nt_length, as_version, source_type)
 
         self.assertFalse(validate_cds_component(gbk))
+
+    def test_bgc_len_filter_below(self):
+        """Tests whether BGCs are correctly filtered out by length constraint"""
+
+        gbk_10 = GBK(Path("gbk_path"), "gbk_hash", 10, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+        gbk_20 = GBK(Path("gbk_path"), "gbk_hash", 20, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+        gbk_30 = GBK(Path("gbk_path"), "gbk_hash", 30, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+
+        gbk_list = [gbk_10, gbk_20, gbk_30]
+
+        BigscapeConfig.MIN_BGC_LENGTH = 25
+
+        filtered_gbks = GBK.length_filter(gbk_list)
+
+        self.assertEqual(len(filtered_gbks), 1)
+
+    def test_bgc_len_filter_none_left(self):
+        """Tests whether BGCs are correctly filtered out by length constraint"""
+
+        gbk_10 = GBK(Path("gbk_path"), "gbk_hash", 10, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+        gbk_20 = GBK(Path("gbk_path"), "gbk_hash", 20, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+        gbk_30 = GBK(Path("gbk_path"), "gbk_hash", 30, "as_version", bs_enums.SOURCE_TYPE.QUERY)
+
+        gbk_list = [gbk_10, gbk_20, gbk_30]
+
+        BigscapeConfig.MIN_BGC_LENGTH = 35
+
+        self.assertRaises(RuntimeError, GBK.length_filter, gbk_list)
+
